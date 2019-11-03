4b6a7da73f2f1e8e4acd9f69eaef0b0f5d63d1c8
jello <2511841570@qq.com>
2019-11-03 19:53:17 +0800

    :sparkles: add antdesign

---
+++ src/header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'antd';
+function Header() {
+  const func = ['登录', '注册', '个人中心']
+  return(
+    <div className="function">
+      
+    </div>
+  )
+}
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Header from './header/index'
 function App() {
-  return(<p>inintial</p>)
+  return(
+    <div>
+      <Header />
+    </div>
+  )
 }
 ReactDOM.render(<App />, document.getElementById('root'));
