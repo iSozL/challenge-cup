c23cae81da20440d2b765bff04a617389bc1636e
jello <2511841570@qq.com>
2019-11-03 19:22:54 +0800

    :tada: initial

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+function App() {
+  return(<p>inintial</p>)
+}
+ReactDOM.render(<App />, document.getElementById('root'));
