670c5f9b33ec07779b10884230d5fb1b62807551
jello <2511841570@qq.com>
2019-11-03 20:08:26 +0800

    :lipstick: add header UI

--- src/header/index.js
+++ src/header/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import { Button } from 'antd';
+import React from "react";
+import { Button, Row, Col, Divider } from "antd";
+import "./index.css";
 function Header() {
-  const func = ['登录', '注册', '个人中心']
-  return(
-    <div className="function">
-      
+  const func = ["登录", "注册", "个人中心"];
+  return (
+    <div>
+      <div className="function">
+        <Row gutter={22}>
+          {func.map((item, index) => (
+            <Col key={index} span={6}>
+              <Button>{item}</Button>
+            </Col>
+          ))}
+        </Row>
+      </div>
+      <Divider />
     </div>
-  )
+  );
 }
-export default Header
+export default Header;
