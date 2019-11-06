1ad9ff0fe3aac111a85053eb882674a8db83472e
jello <2511841570@qq.com>
2019-11-06 16:00:33 +0800

    :lipstick: compelete recommendPost UI

--- src/body/tabBar/forum/recommend.jsx
+++ src/body/tabBar/forum/recommend.jsx
@@ -5,22 +5,71 @@ class RecommendPost extends React.Component {
     super(props);
   }
   render() {
+    const recommendPostData = [
+      {
+        title: "2019中国生物实验室耗材行业发展概况",
+        subtitle: "..........................."
+      },
+      {
+        title: "2019中国生物实验室耗材行业发展概况",
+        subtitle: "..........................."
+      },
+      {
+        title: "2019中国生物实验室耗材行业发展概况",
+        subtitle: "..........................."
+      },
+      {
+        title: "2019中国生物实验室耗材行业发展概况",
+        subtitle: "..........................."
+      },
+      {
+        title: "2019中国生物实验室耗材行业发展概况",
+        subtitle: "..........................."
+      }
+    ];
     return (
-      <Card>
-        <div style={{ fontWeight: "bold", fontSize: "18px" }}>nmsl</div>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <div style={{ fontWeight: "bold", fontSize: "14px" }}>subtitle</div>
-          <Button>阅读全文</Button>
-        </div>
-        <div style={{ display: "flex", justifyContent: "space-between"}}>
-          <Icon type="like" />我赞同
-          <Icon type="dislike" />我反对
-          <Icon type="message" />评论
-          <Icon type="share-alt" />分享
-          <Icon type="folder-open" />收藏
-          <Icon type="heart" />喜欢
-        </div>
-      </Card>
+      <div>
+        {recommendPostData.map((item, index) => (
+          <Card key={index} style={{marginTop: "20px"}}>
+            <div style={{ fontWeight: "bold", fontSize: "18px" }}>
+              {item.title}
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <div style={{ fontWeight: "bold", fontSize: "14px" }}>
+                {item.subtitle}
+              </div>
+              <Button>阅读全文</Button>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "20px",
+                padding: "0 0 0 500px"
+              }}
+            >
+              <div>
+                <Icon type="like" /> 我赞同
+              </div>
+              <div>
+                <Icon type="dislike" /> 我反对
+              </div>
+              <div>
+                <Icon type="message" /> 评论
+              </div>
+              <div>
+                <Icon type="share-alt" /> 分享
+              </div>
+              <div>
+                <Icon type="folder-open" /> 收藏
+              </div>
+              <div>
+                <Icon type="heart" /> 喜欢
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
     );
   }
 }
